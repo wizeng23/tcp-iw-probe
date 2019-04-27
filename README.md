cc19bfd9ac32782cb72ea20f2ac1222470bfab88
William Zeng <wizeng@stanford.edu>
2019-04-26 23:37:14 -0700

    scapy test script; unwanted os rst flags sent

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Some resources because the Scapy Docs aren't too helpful
 http://thepacketgeek.com/scapy-p-06-sending-and-receiving-with-scapy/
 https://stackoverflow.com/questions/4750793/python-scapy-or-the-like-how-can-i-create-an-http-get-request-at-the-packet-leve
 http://www.thice.nl/creating-ack-get-packets-with-scapy/
+https://blogs.sans.org/pen-testing/files/2016/04/ScapyCheatSheet_v0.2.pdf
--- scapy-test.py
+++ scapy-test.py
@@ -1,1 +1,12 @@
 from scapy.all import *
+
+dst = 'www.stanford.edu'
+sport = 1500 
+
+syn = IP(dst=dst) / TCP(sport=sport, dport=80, flags='S')
+syn_ack = sr1(syn)
+getStr = 'GET / HTTP/1.1\r\nHost: %s\r\n\r\n' % dst
+request = IP(dst=dst) / TCP(dport=80, sport=syn_ack[TCP].dport,
+             seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A') / getStr
+reply = sr(request)
+# print ans.summary()
