45bb5f4869eb42d6d4be3cd584423e77e49a3251
William Zeng <wizeng@stanford.edu>
2019-04-27 02:17:15 -0700

    Wrote IW probe code structure

--- scapy-test.py
+++ scapy-test.py
@@ -1,12 +1,13 @@
 from scapy.all import *
 
-dst = 'www.stanford.edu'
-sport = 1500 
+dst = 'cs144.keithw.org'
+sport = 1004
 
 syn = IP(dst=dst) / TCP(sport=sport, dport=80, flags='S')
 syn_ack = sr1(syn)
-getStr = 'GET / HTTP/1.1\r\nHost: %s\r\n\r\n' % dst
+getStr = 'GET /hello HTTP/1.1\r\nHost: %s\r\n\r\n' % dst
 request = IP(dst=dst) / TCP(dport=80, sport=syn_ack[TCP].dport,
              seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A') / getStr
-reply = sr(request)
+ans = sr(request, timeout=1, multi=True)
+# reply = ans[0][0][1]
 # print ans.summary()
