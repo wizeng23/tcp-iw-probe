b1712e43963da3ccd7b4c7c01fbd4bb65f85e4c2
William Zeng <wizeng@stanford.edu>
2019-04-29 03:38:04 -0700

    initial changes for tls plus todos

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # tcp-iw-probe
 Recreating the initial TCP congestion window results of Padhye et. al.'s 2001 paper.
 
+TODOs
+Decrease or remove timeout in sr1 call for synack
+Check how many ip's fail due to dns errors, do a separate dns run?
+TLS? Many fail due to http 302, error 3 or 4
+design doc
+switch to python2
+
 Some resources because the Scapy Docs aren't too helpful
 
 http://thepacketgeek.com/scapy-p-06-sending-and-receiving-with-scapy/
--- scapy-test.py
+++ scapy-test.py
@@ -4,12 +4,33 @@ import random
 
 # file to test invidividual sites
 def main():
-    ip = 'stanford.edu'
+    ip = 'google.com'
     sport = random.randint(1024, 10000)
     print('Using sport %d' % sport)
     reps = 1
     mss = 64
     util.repeat_iw_query(ip, sport, reps, mss, None, None)
+
+# def tls(ip):
+#     with TLSSocket(client=True) as tls_socket:
+#         try:
+#             tls_socket.connect(ip)
+#             print('Connected to server {}'.format(ip))
+#         except tls_socket.timeout:
+#             print('Failed to open connection to server {}'.format(ip))
+#         else:
+#             try:
+#                 server_hello, server_kex = tls_socket.do_handshake(tls_version, ciphers, extensions)
+#                 server_hello.show()
+#             except TLSProtocolError as tpe:
+#                 print('Got TLS error {}'.format(tpe))
+#                 tpe.response.show()
+#             else:
+#                 resp = tls_socket.do_round_trip(TLSPlaintext(data='GET / HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n'.format(ip)))
+#                 print('Got response from server')
+#                 resp.show()
+#             finally:
+#                 print(tls_socket.tls_ctx)
 
 # syn = IP(dst=dst) / TCP(sport=sport, dport=80, flags='S', options=[('MSS', mss)])
 # syn_ack = sr1(syn)
@@ -23,3 +44,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    # tls('google.com')
